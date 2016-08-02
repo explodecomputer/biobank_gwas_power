0c886632b927c8c43d75b20840004a5d46beac9a
Gibran Hemani <explodecomputer@gmail.com>
2016-08-02 14:18:55 +0100

    updated

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(ggthemes)
+library(dplyr)
 
 load("../results/simulations.RData")
 
@@ -19,9 +20,26 @@ labs(x="Discovery sample size (x1000)", y="Power to detect", colour="Simulated\n
 
 
 
+plot_dat <- subset(params, maf==0.5)
+plot_dat$power_change <- plot_dat$total_power - plot_dat$power
+plot_dat$power <- as.factor(plot_dat$power)
+plot_dat$ndiscovery <- plot_dat$ndiscovery / 1000
+
+ggplot(plot_dat, aes(x=ndiscovery, y=power_change, group=power)) +
+geom_line(aes(colour=power)) +
+geom_point(aes(colour=power)) +
+facet_grid(. ~ discovery_threshold) +
+theme_minimal() +
+labs(x="Discovery sample size (x1000)", y="Power to detect", colour="Simulated\npower for\nn=500000")
+
+
+
+
+
 # Simulations
 
-plot_dat <- subset(params, maf==0.5)
+plot_dat <- params %>% group_by(ndiscovery, power, discovery_threshold) %>%
+	dplyr::summarise(simulation_power=mean(simulation_power), simulation_power_t=mean(simulation_power_t))
 plot_dat$power <- as.factor(plot_dat$power)
 plot_dat$ndiscovery <- plot_dat$ndiscovery / 1000
 
@@ -32,4 +50,19 @@ facet_grid(. ~ discovery_threshold) +
 theme_minimal() +
 labs(x="Discovery sample size (x1000)", y="Power to detect", colour="Simulated\npower for\nn=500000")
 
+
+
+
+plot_dat <- params %>% group_by(ndiscovery, power, discovery_threshold) %>%
+	dplyr::summarise(simulation_power=mean(simulation_power), simulation_power_t=mean(simulation_power_t))
+plot_dat$power_change <- plot_dat$simulation_power - plot_dat$simulation_power_t
+plot_dat$power <- as.factor(plot_dat$power)
+plot_dat$ndiscovery <- plot_dat$ndiscovery / 1000
+
+ggplot(plot_dat, aes(x=ndiscovery, y=power_change, group=power)) +
+geom_line(aes(colour=power)) +
+geom_point(aes(colour=power)) +
+facet_grid(. ~ discovery_threshold) +
+theme_minimal() +
+labs(x="Discovery sample size (x1000)", y="Power to detect", colour="Simulated\npower for\nn=500000")
 
